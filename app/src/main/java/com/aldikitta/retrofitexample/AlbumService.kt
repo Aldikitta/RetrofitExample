3f5ca65c0fc76798eded00f9143a04bfee2198ef
Aldikitta <andi.kitta77@gmail.com>
2022-05-23 22:34:24 +0800

    Initial commit

---
+++ app/src/main/java/com/aldikitta/retrofitexample/AlbumService.kt
@@ -0,0 +1,9 @@
+package com.aldikitta.retrofitexample
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AlbumService {
+    @GET("/albums")
+    suspend fun getAlbums(): Response<Albums>
+}
---
+++ app/src/main/java/com/aldikitta/retrofitexample/Albums.kt
@@ -0,0 +1,3 @@
+package com.aldikitta.retrofitexample
+
+class Albums : ArrayList<AlbumsItem>()
